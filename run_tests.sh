83153b05a7c4d25d918cc191e5ded9c6bb85f69f
Mike Samuel <mikesamuel@gmail.com>
2011-12-22 20:19:03 -0500

    dinked script to make it easier to see which tests are running

--- run_tests.sh
+++ run_tests.sh
@@ -3,5 +3,8 @@
 export DIR="$(dirname "$0")"
 
 for testmodule in tests/*_test.py; do
+    echo
+    echo $testmodule
+    echo $testmodule | tr ' -~' '='
     PYTHONPATH="$DIR/src:$DIR/tests:$PYTHONPATH" python "$testmodule"
 done
