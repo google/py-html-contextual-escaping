d4bf4865cc42c498d766d9f416b842f3d23efdb1
Mike Samuel <mikesamuel@gmail.com>
2011-12-28 10:40:43 -0500

    added test filtering via the TESTFILTER env variable.  Usage: TESTFILTER='<a regex>' ./run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# Usage:
+#   TESTFILTER='<a regex>' ./run_tests.sh
+# The test filter is optional and is a regular expression in egrep format that
+# should match the name of py files under tests.
+
 export DIR="$(dirname "$0")"
 
 for testmodule in tests/*_test.py; do
-    echo
-    echo $testmodule
-    echo $testmodule | tr ' -~' '='
-    PYTHONPATH="$DIR/src:$DIR/tests:$PYTHONPATH" python "$testmodule"
+    if [ -z "$TESTFILTER" ] || (echo "$testmodule" | egrep -q "$TESTFILTER")
+    then
+	echo
+	echo $testmodule
+	echo $testmodule | tr ' -~' '='
+	PYTHONPATH="$DIR/src:$DIR/tests:$PYTHONPATH" python "$testmodule"
+    fi
 done
