945e004fe8a8c8f04095519a0206cce0e2c3b360
Mike Samuel <mikesamuel@gmail.com>
2011-12-28 16:44:24 -0500

    added code to run_tests.sh to run coverage.py

--- run_tests.sh
+++ run_tests.sh
@@ -7,12 +7,26 @@
 
 export DIR="$(dirname "$0")"
 
+if [ -z "$PYTHON" ]; then
+    if [ -n "$(which coverage-2.7)" ]; then
+	rm -f .coverage
+	export PYTHON="coverage-2.7 run -a --branch --source=$DIR/src"
+    else
+	export PYTHON="python"
+    fi
+fi
+
 for testmodule in tests/*_test.py; do
     if [ -z "$TESTFILTER" ] || (echo "$testmodule" | egrep -q "$TESTFILTER")
     then
 	echo
 	echo $testmodule
 	echo $testmodule | tr ' -~' '='
-	PYTHONPATH="$DIR/src:$DIR/tests:$PYTHONPATH" python "$testmodule"
+	PYTHONPATH="$DIR/src:$DIR/tests:$PYTHONPATH" $PYTHON "$testmodule"
     fi
 done
+
+if [ -e .coverage ]; then
+    mkdir -p test_output
+    coverage-2.7 html -d test_output/coverage
+fi
